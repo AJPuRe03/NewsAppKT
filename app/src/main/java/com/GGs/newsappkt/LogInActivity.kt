d346c95bc2c98fea6d61707b06a8c2be0521c9d9
angelPuga03 <angelpure2003@gmail.com>
2023-05-07 17:46:23 -0600

    Diseño y codificacion de LogIn implementados (LogIn completo!)

--- app/src/main/java/com/GGs/newsappkt/LogInActivity.kt
+++ app/src/main/java/com/GGs/newsappkt/LogInActivity.kt
@@ -1,11 +1,74 @@
 package com.GGs.newsappkt
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LogInActivity : AppCompatActivity() {
+
+    lateinit var txtEmail: EditText
+    lateinit var txtContra: EditText
+    lateinit var btnLogIn: Button
+    lateinit var auth: FirebaseAuth
+    lateinit var pbCargando: ProgressBar
+    lateinit var txvSignUpNow: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        txtEmail = findViewById(R.id.txtEmailLI)
+        txtContra = findViewById(R.id.txtContraLI)
+        btnLogIn = findViewById(R.id.btnLogIn)
+        auth = Firebase.auth
+        pbCargando = findViewById(R.id.pbCargandoLI)
+        txvSignUpNow = findViewById(R.id.txvSignUpNow)
+
+        txvSignUpNow.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(view: View) {
+                val intent = Intent(applicationContext, SignUpActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        })
+        btnLogIn.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(view: View) {
+                pbCargando.visibility = View.VISIBLE
+                val email = txtEmail.text.toString()
+                val contra = txtContra.text.toString()
+
+                if (email == "" || contra == ""){
+                    Toast.makeText(this@LogInActivity, "Llena todos los campos", Toast.LENGTH_SHORT).show()
+                    pbCargando.visibility = View.GONE
+                    return
+                }
+
+                auth.signInWithEmailAndPassword(email, contra)
+                    .addOnCompleteListener(this@LogInActivity) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(baseContext, "Inicio de Sesión exitoso",
+                                Toast.LENGTH_SHORT).show()
+                            val intent = Intent(applicationContext, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(baseContext, "Usuario o contraseña incorrectos",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            }
+
+        })
     }
 }
--- app/src/main/java/com/GGs/newsappkt/SignUpActivity.kt
+++ app/src/main/java/com/GGs/newsappkt/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.GGs.newsappkt
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -22,6 +24,7 @@ class SignUpActivity : AppCompatActivity() {
     lateinit var btnReg: Button
     lateinit var auth: FirebaseAuth
     lateinit var pbCargando: ProgressBar
+    lateinit var txvLogInNow: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -32,6 +35,16 @@ class SignUpActivity : AppCompatActivity() {
         btnReg = findViewById(R.id.btnRegistrar)
         auth = Firebase.auth
         pbCargando = findViewById(R.id.pbCargandoRegistro)
+        txvLogInNow = findViewById(R.id.txvLogInNow)
+
+        txvLogInNow.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(view: View) {
+                val intent = Intent(applicationContext, LogInActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        })
 
         btnReg.setOnClickListener(object: View.OnClickListener {
             override fun onClick(view: View) {
@@ -43,10 +56,17 @@ class SignUpActivity : AppCompatActivity() {
 
                 if (usuario == "" || email == "" || contra == "" || reContra == ""  ){
                     Toast.makeText(this@SignUpActivity, "Llena todos los campos", Toast.LENGTH_SHORT).show()
+                    pbCargando.visibility = View.GONE
+                    return
+                }
+                if (contra.length < 6){
+                    Toast.makeText(this@SignUpActivity, "La contraseña debe tener al menos 6 dígitos", Toast.LENGTH_SHORT).show()
+                    pbCargando.visibility = View.GONE
                     return
                 }
                 if (contra != reContra){
                     Toast.makeText(this@SignUpActivity, "Verifica que las contraseñas sean iguales", Toast.LENGTH_SHORT).show()
+                    pbCargando.visibility = View.GONE
                     return
                 }
 
